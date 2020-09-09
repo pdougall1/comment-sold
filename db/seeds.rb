7a02270903a8bce11618dcd239e024bc9bad1c3f
Patrick Dougall <pdougall1@gmail.com>
2020-09-08 23:06:36 -0400

    Delete resource before seeding it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,15 @@ dir = Rails.root.join('db', 'seed_data')
 # Creating the rows one at a time lets Rails validations work to maintian data integrity
 # But the seed takes a minute :/
 
+
 Dir.foreach(dir) do |filename|
   next if filename == '.' or filename == '..'
 
   resource_name = filename.split('.').first
   resource = resource_name.singularize.camelize.constantize
   rows = []
+
+  resource.delete_all
 
   CSV.parse(File.read("#{dir}/#{filename}"), headers: true) do |row|
     if resource == User
